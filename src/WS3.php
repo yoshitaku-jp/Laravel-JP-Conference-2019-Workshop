2be963f545a3b16e2df1f6a0b95b8e7d9ba116ca
Takuya YOSHIMURA <yoshitaku-jp@users.noreply.github.com>
2019-01-29 15:36:09 +0900

    FizzBuzzについて #2 の指摘事項を反映
    $num % 5 == 00　を 0 に
    比較を　===　に
    $result .= "0";　を返すのではなく、数字を返す

--- src/WS3.php
+++ src/WS3.php
@@ -8,16 +8,16 @@ class WS3 {
 
         $result = "";
 
-        if($num % 3 == 0){
+        if($num % 3 === 0){
             $result .= "Fizz";
         }
 
-        if($num % 5 == 0){
+        if($num % 5 === 0){
             $result .= "Buzz";
         }
 
-        if(!($num % 3 == 0) && !($num % 5 == 00)){
-            $result .= "0";
+        if(!($num % 3 === 0) && !($num % 5 === 0)){
+            $result = $num;
         }
         return $result;
     }
